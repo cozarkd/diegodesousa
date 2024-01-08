1b30cc4f836c59867f49084f5e511ce7f57a4774
cozarkd <cozarkd@gmail.com>
2024-01-08 19:55:00 +0100

    initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Diego de Sousa | frontend developer';
+export const SITE_DESCRIPTION = 'Desarrollador web que trabaja con startups y proyectos creativos desde el cariño y la inmersión. Desde la conceptualización hasta la experiencia de usuario; y más allá.';
