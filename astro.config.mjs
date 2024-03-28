5b2b2cb8f90a8ddff194fdd5854539fe49023c25
Diego de Sousa <cozarkd@gmail.com>
2024-03-28 19:48:11 +0100

    misc changes from nas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import tailwind from "@astrojs/tailwind";
+import tailwindcssNesting from 'tailwindcss/nesting'
+
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://diegodesousa.com',
-	integrations: [mdx(), sitemap(), tailwind({
-		nesting: true
-	})]
-})
+	integrations: [mdx(), sitemap(), tailwind()],
+	server: { port: 3000 },
+	vite: {
+		css: {
+			postcss: {
+				plugins: [
+					tailwindcssNesting(),
+				],
+			},
+		},
+	},
+});
