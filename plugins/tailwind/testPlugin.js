999656f0bde0dc449886edbd69f3cd0e4eb67d7c
Diego de Sousa <cozarkd@gmail.com>
2024-06-20 01:56:07 +0200

    clean repo and blog directories

--- plugins/tailwind/testPlugin.js
+++
@@ -1,9 +0,0 @@
-import plugin from 'tailwindcss/plugin'
-
-const testPlugin = plugin(function ({ addBase }) {
-	addBase({
-		body: { backgroundColor: 'purple' }
-	})
-})
-
-module.exports = testPlugin
