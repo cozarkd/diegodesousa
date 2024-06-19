87c9044762953503bbd1c0477e4785c9d487b16c
Diego de Sousa <cozarkd@gmail.com>
2024-06-19 22:47:34 +0200

    Alfa

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -50,6 +50,16 @@ export default {
 					foreground: 'hsl(var(--card-foreground))'
 				}
 			},
+			dropShadow: {
+				'3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+				'4xl': [
+					'0 35px 35px rgba(0, 0, 0, 0.25)',
+					'0 45px 65px rgba(0, 0, 0, 0.15)'
+				]
+			},
+			boxShadow: {
+				'3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+			},
 			borderRadius: {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
