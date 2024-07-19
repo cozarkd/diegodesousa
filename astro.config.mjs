720167004aa2ffb7c4b4ee412bc9a0061f791a3a
Diego de Sousa <cozarkd@gmail.com>
2024-07-19 02:50:43 +0200

    sitemap, robots and misc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,29 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
-import tailwindcssNesting from 'tailwindcss/nesting'
+import tailwindcssNesting from 'tailwindcss/nesting';
+import robotsTxt from 'astro-robots-txt';
+
+import robotsConfig from './robots-txt.config';
 
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://diegodesousa.com',
-	integrations: [mdx(), sitemap(), tailwind()],
+	integrations: [mdx(), sitemap(), robotsTxt(robotsConfig), tailwind()],
 	i18n: {
 		defaultLocale: "es",
-		locales: ["es", "en", "pt"],
+		locales: ["es", "en", "pt"]
 	},
-	server: { port: 3000 },
+	server: {
+		port: 3000
+	},
 	vite: {
 		css: {
 			postcss: {
-				plugins: [
-					tailwindcssNesting(),
-				],
-			},
-		},
-	},
+				plugins: [tailwindcssNesting()]
+			}
+		}
+	}
 });
